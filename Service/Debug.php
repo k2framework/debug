ff82b7f67cba38a520e652e72048e378c262932e
Manuel Aguirre <programador.manuel@gmail.com>
2012-11-07 13:37:18 -0430

    Siguen los agregados al K2_Debug...
    ahora guarda los queries en sesión, por si hay un redirect, se puedan
    obtener las consultas de la petición anterior.
    
    Ademas hace uso del servicio @view para crear el banner con la info
    para el debug.
    
    Por ahora solo muestra los queries

--- Service/Debug.php
+++ Service/Debug.php
@@ -2,7 +2,9 @@
 
 namespace K2\Debug\Service;
 
+use KumbiaPHP\View\View;
 use KumbiaPHP\Kernel\Event\ResponseEvent;
+use KumbiaPHP\Kernel\Session\SessionInterface;
 use KumbiaPHP\ActiveRecord\Event\AfterQueryEvent;
 use KumbiaPHP\ActiveRecord\Event\BeforeQueryEvent;
 
@@ -17,9 +19,48 @@ class Debug
     protected $queries;
     protected $queryTimeInit;
 
+    /**
+     *
+     * @var View 
+     */
+    protected $view;
+
+    /**
+     *
+     * @var SessionInterface 
+     */
+    protected $session;
+
+    function __construct(View $view, SessionInterface $session)
+    {
+        $this->view = $view;
+        $this->session = $session;
+    }
+
     public function onResponse(ResponseEvent $event)
     {
-        //var_dump($this->queries);die;
+        if (function_exists('mb_stripos')) {
+            $posrFunction = 'mb_strripos';
+            $substrFunction = 'mb_substr';
+        } else {
+            $posrFunction = 'strripos';
+            $substrFunction = 'substr';
+        }
+
+        $response = $event->getResponse();
+        $content = $response->getContent();
+
+        if (false !== $pos = $posrFunction($content, '</body>')) {
+
+            $html = $this->view->render('K2/Debug:banner', null, array(
+                        'queries' => $this->session->all('k2_debug_queries')
+                    ))->getContent();
+            
+            $this->session->delete(null,'k2_debug_queries');
+
+            $content = $substrFunction($content, 0, $pos) . $html . $substrFunction($content, $pos);
+            $response->setContent($content);
+        }
     }
 
     public function onBeforeQuery(BeforeQueryEvent $event)
@@ -29,18 +70,20 @@ class Debug
 
     public function onAfterQuery(AfterQueryEvent $event)
     {
-        //$this->addQuery($event, microtime() - $this->queryTimeInit);
+        $this->addQuery($event, microtime() - $this->queryTimeInit);
     }
 
     protected function addQuery(AfterQueryEvent $event, $runtime)
     {
-        $this->queries[] = array(
+        //$this->queries[] = array(
+        $data = array(
             'runtime' => $runtime,
             'query' => $event->getQuery(),
             'parameters' => $event->getParameters(),
             'type' => $event->getQueryType(),
             'result' => $event->getResult(),
         );
+        $this->session->set(md5(microtime()), $data, 'k2_debug_queries');
     }
 
 }
